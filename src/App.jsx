a27b996da24b8a89dae45042ebdc9e1b49d38420
Niloy4001 <huzaifasikderniloy58bd@gmail.com>
2024-10-22 11:12:35 +0600

    header section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
 
@@ -7,6 +8,11 @@ function App() {
   return (
     <>
       
+    <div className='w-[90%] mx-auto'>
+    <Header></Header>
+    
+    </div>
+
       
     </>
   )
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import profile from '../../assets/profile.png'
+
+const Header = () => {
+    return (
+        <div className='flex justify-between items-center py-8 lg:py-16'>
+            <h1 className="font-bold text-[40px] text-[#111111]">Knowledge Cafe</h1>
+            <div>
+                <img src={profile} alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default Header;
